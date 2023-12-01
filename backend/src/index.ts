4b333ab44cc60c738609221f48cbdf3dfa35f6cf
Pradeep Kundekar <pradeepkundekar1010@gmail.com>
2023-12-01 23:57:24 +0530

    Prisma setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import {ApolloServer} from '@apollo/server';
 import {expressMiddleware} from '@apollo/server/express4'
 import bodyParser from 'body-parser';
+//@ts-ignore
+import { prismaClient } from '../libs/db';
 
 async function init()
 {
@@ -16,11 +18,33 @@ async function init()
                 getGreet:String
                 getGreetByName(name:String):String
             }
+            type Mutation{
+                createUser( firstName:String!,lastName:String!,email:String!,password:String!,salt:String! ):Boolean
+            }
         `,
         resolvers:{
             Query:{
                 getGreet:()=>("Hello, from graphql"),
                 getGreetByName:(_,{name})=>(`Hello ${name} `)
+            },
+            Mutation:{
+                createUser:async (_,{firstName,lastName,email,password,salt}
+                    :
+                {
+                    firstName:string;
+                    lastName:string;
+                    email:string;
+                    password:string;
+                    salt:string}
+                )=>{ prismaClient.user.create({
+                    data:
+                    {
+                        email,
+                        firstName,
+                        lastName,
+                        password,
+                        salt:'random'
+                    }}) }
             }
         }
     });
